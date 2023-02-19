4772acc03c8b9a1985bd0855dc5565c352923ada
NicholasLor <104872171+NicholasLor@users.noreply.github.com>
2023-02-18 22:52:25 -0700

    Delete game directory

--- game/src/main/java/com/whoheskatefor/game/repository/playerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.whoheskatefor.game.repository;
-
-import com.whoheskatefor.game.entity.player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface playerRepository extends JpaRepository<player,Long> {
-
-
-}
