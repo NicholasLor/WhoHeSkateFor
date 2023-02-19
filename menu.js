ca40342eb81bdf6eeffb57138611f36467c455f0
Nicholas Lor <bobbylor00@gmail.com>
2023-02-18 22:59:03 -0700

    change file locs

---
+++ menu.js
@@ -0,0 +1,20 @@
+
+
+var cropPlayerJerseysCheckBox = document.getElementById('gridCheck1');
+var startGameButton = document.getElementById('startGameButton');
+
+RedirectWithSettingsApplied();
+
+function RedirectWithSettingsApplied(){
+    startGameButton.addEventListener('click',event => {
+  
+        if (cropPlayerJerseysCheckBox.checked){
+            window.location.href = "game.html?cropPlayerJerseys=true";
+        } else{
+            window.location.href = "game.html";
+        }
+
+  
+    })
+  }
+
